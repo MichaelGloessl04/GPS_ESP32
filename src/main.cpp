f724a44972ed62a0d2a74a4428bcfd8914ecd059
DavidTorbics20 <torbic190220@sr.htlweiz.at>
2023-10-25 13:04:18 +0200

    can get time

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
-/*#include <HardwareSerial.h>
+#include <HardwareSerial.h>
+#include <ESP32Time.h>
+#include <TinyGPSPlus.h>
 
 HardwareSerial serial_port(2); // use UART2
+TinyGPSPlus gps;
+ESP32Time rtc(3600);  // offset in seconds GMT+1
 
 
-$GPGGA: 3D location and accuracy data
+// $GPGGA: 3D location and accuracy data
 
-
+int hour = 0;
+int minute = 0;
+int second = 0;
 
 void setup()  
 {
@@ -14,104 +20,36 @@ void setup()
   Serial.write("initializing GPS...");
   serial_port.begin(9600, SERIAL_8N1, 16, 17);
   Serial.write("Initialized");
+
+  // set the time to the time i get from the gps (hour +2)
+}
+
+void setTime(){
+
+  if (gps.encode(serial_port.read())){
+    hour = gps.time.hour() + 1;
+    minute = gps.time.minute();
+    second = gps.time.second();
+    Serial.print(hour);
+    Serial.print(",");
+    Serial.print(minute);
+    Serial.print(",");
+    Serial.println(second);
+
+  }
+
+  rtc.setTime(second, minute, hour, 17, 11, 2023);
 }
 
 void loop(){
   while (serial_port.available() > 0){
     // get the byte data from the GPS
-    uint8_t gpsData = serial_port.read();
-    Serial.write(gpsData);
+    // uint8_t gpsData = serial_port.read();
+    // Serial.write(gpsData);
+
+    setTime();
   }
-}
-*/
-
-#include <TinyGPSPlus.h>
-/* 
-   This sample sketch should be the first you try out when you are testing a TinyGPSPlus
-   (TinyGPSPlus) installation.  In normal use, you feed TinyGPSPlus objects characters from
-   a serial NMEA GPS device, but this example uses static strings for simplicity.
-*/
-
-// A sample NMEA stream.
-const char *gpsStream =
-  "$GPRMC,045103.000,A,3014.1984,N,09749.2872,W,0.67,161.46,030913,,,A*7C\r\n"
-  "$GPGGA,045104.000,3014.1985,N,09749.2873,W,1,09,1.2,211.6,M,-22.5,M,,0000*62\r\n"
-  "$GPRMC,045200.000,A,3014.3820,N,09748.9514,W,36.88,65.02,030913,,,A*77\r\n"
-  "$GPGGA,045201.000,3014.3864,N,09748.9411,W,1,10,1.2,200.8,M,-22.5,M,,0000*6C\r\n"
-  "$GPRMC,045251.000,A,3014.4275,N,09749.0626,W,0.51,217.94,030913,,,A*7D\r\n"
-  "$GPGGA,045252.000,3014.4273,N,09749.0628,W,1,09,1.3,206.9,M,-22.5,M,,0000*6F\r\n";
-
-// The TinyGPSPlus object
-TinyGPSPlus gps;
-
-void setup()
-{
-  Serial.begin(115200);
-
-  Serial.println(F("BasicExample.ino"));
-  Serial.println(F("Basic demonstration of TinyGPSPlus (no device needed)"));
-  Serial.print(F("Testing TinyGPSPlus library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
-  Serial.println(F("by Mikal Hart"));
-  Serial.println();
-
-  Serial.println();
-  Serial.println(F("Done."));
-}
-
-
-void displayInfo()
-{
-  Serial.print(F("Location: ")); 
-  if (gps.location.isValid())
-  {
-    Serial.print(gps.location.lat(), 6);
-    Serial.print(F(","));
-    Serial.print(gps.location.lng(), 6);
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial.print(F("  Date/Time: "));
-  if (gps.date.isValid())
-  {
-    Serial.print(gps.date.month());
-    Serial.print(F("/"));
-    Serial.print(gps.date.day());
-    Serial.print(F("/"));
-    Serial.print(gps.date.year());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial.print(F(" "));
-  if (gps.time.isValid())
-  {
-    if (gps.time.hour() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.hour());
-    Serial.print(F(":"));
-    if (gps.time.minute() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.minute());
-    Serial.print(F(":"));
-    if (gps.time.second() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.second());
-    Serial.print(F("."));
-    if (gps.time.centisecond() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.centisecond());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial.println();
-}
-
-
-void loop()
-{
-      displayInfo();
+  // Serial.println(serial_port);
+  Serial.print(rtc.getTime("%A, %B %d %Y %H:%M:%S, "));
+  Serial.println(rtc.getMillis());
 }
