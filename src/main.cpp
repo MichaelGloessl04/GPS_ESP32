e8acd909de96ab1f8494c6b607db6126a40dce4b
DavidTorbics20 <torbic190220@sr.htlweiz.at>
2023-11-06 17:26:53 +0100

    main can send time

--- src/main.cpp
+++ src/main.cpp
@@ -36,7 +36,7 @@ void setup()
   pinMode(lichtschrnake, INPUT);
   wifi.autoConnect("Lichtschranken Wifi", "LichtschrankenPWD");
 
-  mqtt.setClientName("Test");
+  mqtt.setClientName("Test_B");
 }
 
 void setTime(){
@@ -82,9 +82,7 @@ void loop(){
   if (started){
     // Serial.println(serial_port);
     myTime = rtc.getTime("%A, %B %d %Y %H:%M:%S:") + rtc.getMillis();
-    mqtt.client.publish("AAA", myTime.c_str());
-    Serial.println(myTime);
+    mqtt.client.publish("BBB", myTime.c_str(), 2);
+    // Serial.println(myTime);
   }
-
-  delay(1000);
 }
