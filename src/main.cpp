463e669dda84b9f1b7659dee20d8aa3a0039f98f
DavidTorbics20 <torbic190220@sr.htlweiz.at>
2023-11-08 10:47:15 +0100

    refactoring main.ccp to work with freeRTOS

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,12 @@
 #include <ESP32Time.h>
 #include <TinyGPSPlus.h>
 #include <WiFiManager.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 const char* ADR = "192.168.88.229";
 
-int counter = 0;
+TaskHandle_t timeTaskHandle = NULL;
 
 HardwareSerial serial_port(2); // use UART2
 TinyGPSPlus gps;  
@@ -25,6 +27,46 @@ bool started = false;
 
 String myTime;
 
+void setTime(){
+
+  if (gps.encode(serial_port.read())){
+    hour = gps.time.hour();
+    minute = gps.time.minute();
+    second = gps.time.second();
+  }
+
+  /* Serial.print(hour);
+  Serial.print(",");
+  Serial.print(minute);
+  Serial.print(",");
+  Serial.println(second); */  
+
+  rtc.setTime(second, minute, hour, 17, 11, 2023);
+}
+
+void recordTime(void *args) {
+  while (1){
+
+    while (serial_port.available() > 0){
+      setTime();
+    }
+
+    reading = digitalRead(lichtschrnake); 
+
+    if (reading == HIGH && previous == LOW){
+      started = !started;
+    }
+
+    previous = reading;
+
+    if (started){
+      myTime = rtc.getTime("%A, %B %d %Y %H:%M:%S:") + rtc.getMillis();
+      mqtt.client.publish("BBB", myTime.c_str(), 2);
+      Serial.println(myTime);
+    }
+  }
+}
+
 void setup()  
 {
   Serial.begin(115200);
@@ -37,52 +79,12 @@ void setup()
   wifi.autoConnect("Lichtschranken Wifi", "LichtschrankenPWD");
 
   mqtt.setClientName("Test_B");
-}
 
-void setTime(){
-
-  if (gps.encode(serial_port.read())){
-    hour = gps.time.hour();
-    minute = gps.time.minute();
-    second = gps.time.second();
-    Serial.print(hour);
-    Serial.print(",");
-    Serial.print(minute);
-    Serial.print(",");
-    Serial.println(second);
-
-  }
-
-  rtc.setTime(second, minute, hour, 17, 11, 2023);
+  xTaskCreatePinnedToCore( recordTime, "TimeRecorder", 4096, NULL, 25, &timeTaskHandle, 1);
 }
 
 void loop(){
   if (!mqtt.client.connected()){
     mqtt.reconnect();
-  }
-
-  while (serial_port.available() > 0){
-    // get the byte data from the GPS
-    // uint8_t gpsData = serial_port.read();
-    // Serial.write(gpsData);
-
-    // do the synchronization
-
-    setTime();
-  }
-
-  reading = digitalRead(lichtschrnake); 
-
-  if (reading == HIGH && previous == LOW){
-    started = !started;
-  }
-
-  previous = reading;
-
-  if (started){
-    // Serial.println(serial_port);
-    myTime = rtc.getTime("%A, %B %d %Y %H:%M:%S:") + rtc.getMillis();
-    mqtt.client.publish("BBB", myTime.c_str(), 2);
-    // Serial.println(myTime);
   }
 }
